fbc956e16f871ed54e6a71c6c4ffcd1d9bfefcf2
Bruno Malagoli <bmalagoli2@gmail.com>
2022-10-15 15:34:47 -0300

    DataContext and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import DataContextProvider from "./contexts/DataContext/DataContextProvider";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <>
-      <MainPage />
+      <DataContextProvider>
+        <MainPage />
+      </DataContextProvider>
     </>
   );
 }
